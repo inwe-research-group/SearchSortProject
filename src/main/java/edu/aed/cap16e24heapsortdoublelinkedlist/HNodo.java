99dc423b48f27428cc77e8614decea0b127d2901
inwe-research-group <zmamanir@unmsm.edu.pe>
2023-10-21 17:24:27 -0500

    Add cap16e24heapsortdoublelinkedlist package

---
+++ src/main/java/edu/aed/cap16e24heapsortdoublelinkedlist/HNodo.java
@@ -0,0 +1,13 @@
+package edu.aed.cap16e24heapsortdoublelinkedlist;
+
+public class HNodo {
+    int data;
+    HNodo prev;
+    HNodo next;
+    
+    public HNodo(int data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
